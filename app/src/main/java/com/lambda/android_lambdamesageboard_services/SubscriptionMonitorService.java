9ed7ccce03e6e10c6ad7f9fdd2091edad037ab9d
Shoonsk8 <shoon@att.net>
2019-04-11 18:31:30 -0400

    Imposssible to make onCreate, onStartCommand, and onDestroy

---
+++ app/src/main/java/com/lambda/android_lambdamesageboard_services/SubscriptionMonitorService.java
@@ -0,0 +1,20 @@
+package com.lambda.android_lambdamesageboard_services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.view.View;
+
+public class SubscriptionMonitorService extends Service {
+    public SubscriptionMonitorService() {
+    }
+View.OnCreateContextMenuListener
+    OnStartCommand
+        PreferenceManager.OnActivityDestroyListener
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+
+    }
+}
